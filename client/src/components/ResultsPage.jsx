0665055ea2cee5aefde4dbf11eb7c5fa1314ec5c
Seiji Emery <seijiemery@gmail.com>
2018-11-04 08:37:07 -0800

    setting colors (no css)

--- client/src/components/ResultsPage.jsx
+++ client/src/components/ResultsPage.jsx
@@ -8,6 +8,12 @@ class ResultsPage extends Component {
       (concept) => { results[concept.name] = concept.value; }
     ]);
 
+    let hc = ((results["harassment"] * 255)|0).toString(16);
+    let sc = ((results["safe"] * 255)&255).toString(16);
+    if (hc.length == 1) hc = '0'+hc;
+    if (sc.length == 1) sc = '0'+sc;
+    hc = "#"+hc+"0000";
+    sc = "#00"+sc+"00";
     return (
       <div class="message-box">
         <img 
@@ -17,8 +23,12 @@ class ResultsPage extends Component {
         <p>id: {this.props.result.id}</p>
         <p>created: {this.props.result.created_at}</p>
         <h1>Results</h1>
-        <h3>Harassment: {results["harassment"]}</h3>
-        <h3>Safe: {results["safe"]}</h3>
+        <ul>
+          {results["harassment"] > 0.1 && <li class='harass-display' style={{'background-color': hc }}>Harassment</li>}
+          {results["safe"] > 0.1 && <li class='harass-display' style={{'background-color': sc }}>Safe</li>}
+        </ul>
+        {/* <h3>Harassment: {results["harassment"]}</h3>
+        <h3>Safe: {results["safe"]}</h3> */}
         <h1>Confirm?</h1>
         <div class="h-span">
             <button onClick={this.props.onConfirm}>confirm</button>
